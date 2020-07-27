c8f3ad19563bc5e181ff5a86c691c8b6f5c39668
BloodlessRed <58694725+BloodlessRed@users.noreply.github.com>
2020-07-27 16:39:59 +0400

    Added a basic version of routing to our web-application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.scss';
 import TestComponent from "./components/test/test.component";
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import AccountPage from './components/test/test.newpage';
 
 class App extends React.Component{
   render() {
     return (
         <div className="App">
-          <TestComponent/>
+          <BrowserRouter>
+            <Switch>
+              <Route path = '/languagePick' component = {TestComponent}/>
+              <Route path='/account' component = {AccountPage}/>
+            </Switch>
+          </BrowserRouter>
         </div>
     );
   }

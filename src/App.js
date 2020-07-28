5450874584c8639a2cd641b1f7a6d11605557459
AmiliaRei <AmiliaRei@users.noreply.github.com>
2020-07-28 15:39:09 +0400

    к

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.scss';
 import TestComponent from "./components/test/test.component";
+import RegistrationComponent from "./components/Registration/registration.component";
+import AuthorizationComponent from "./components/Authorization/authorization.component";
 
 class App extends React.Component{
   render() {
     return (
         <div className="App">
           <TestComponent/>
+          <RegistrationComponent/>
+          <AuthorizationComponent/>
         </div>
     );
   }
---
+++ src/components/Authorization/authorization.component.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./authorization.component.scss"
+
+class AuthorizationComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="authorization-component__title">
+                <h1>Вход</h1>
+                </div>
+                <p>
+                    <label htmlFor="Имя пользователи" className="uname"> Введите электронную почту</label>
+                    <input id="username" name="username" required="Обязательное" type="text"
+                           placeholder="mymail@mail.com"/>
+                </p>
+                <p>
+                    <label htmlFor="Пароль" className="youpasswd">Введите пароль</label>
+                    <input id="password" name="password" required="Обязательное" type="password"/>
+                </p>
+                <div className="authorization-component__button">
+                <p><input type="submit" value="Вход" />
+                <input type="submit" value="Регистрация" /></p>
+                <p><input type="submit" value="Забыли пароль?" /></p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AuthorizationComponent;
